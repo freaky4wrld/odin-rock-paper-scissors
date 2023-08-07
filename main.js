02bc0a1b2a6f4d161318dd181fadb5ef2284f30d
freaky4wrld <freaky4wrld@gmail.com>
2023-08-07 16:44:39 +0530

    added comments to provide easy and simple understanding of code

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
-const choices = ["rock", "paper", "scissors"]; 
+const choices = ["rock", "paper", "scissors"];  //computer choices array
 
+//function to let compute pick its hand
 function getComputerChoice(){
     let index = Math.floor(Math.random() * choices.length);
     return choices[index];
 }
+//globally accessible computer choice
 const computerSelection = getComputerChoice()
 
+// funtion to evaluate a single round results
 function singleRound(computerSelection,playerSelection) {
+    // case-insensitive choices
     computerSelection = computerSelection.toLowerCase();
     playerSelection = playerSelection.toLowerCase();
     
+    //result evaluation
     if (computerSelection!==playerSelection) {
         switch (playerSelection) {
             case "rock":
@@ -42,20 +47,24 @@ function singleRound(computerSelection,playerSelection) {
                 }
                 break;
             
-    
+            // if a valid value isn't provided
             default:
                 return "Please enter a valid choice"
                 break;
         }
     }
+    // if it's a tie
     else if (playerSelection==computerSelection){
         return "It's a Tie! You got one more attempt"
     }
 }
-
+//function to let the user enter it's choice and evaluate the result after 5 singleRound
 function gamePlay(){
+    //initial scores of the players
     let computerScore = 0;
     let playerScore = 0;
+
+    // to make them play 5 rounds
     for(i=0; i<5; i++){
         playerSelection = prompt("Enter your choice!!");
         let result = singleRound(computerSelection,playerSelection);
@@ -66,6 +75,7 @@ function gamePlay(){
             playerScore++;
         }
     }
+    //final result evaluation
     let finalResult = (computerScore>playerScore) ? `Computer won the match | Computer Score: ${computerScore}| Player Score: ${playerScore}`: `Player won the match | Player Score: ${playerScore}| Computer Score: ${computerScore}`;
     return finalResult
 }
